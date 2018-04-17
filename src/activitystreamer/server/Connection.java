43ba30f297bfaf9a973f25fcc353151b792001be
Wielil <38398751+Wielil@users.noreply.github.com>
2018-04-17 11:52:56 +1000

    Update Connection.java

--- src/activitystreamer/server/Connection.java
+++ src/activitystreamer/server/Connection.java
@@ -12,6 +12,11 @@ import java.net.Socket;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+//wei
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 import activitystreamer.util.Settings;
 
 
@@ -23,10 +28,10 @@ public class Connection extends Thread {
 	private PrintWriter outwriter;
 	private boolean open = false;
 	private Socket socket;
-	private boolean term=false;
+	private boolean term = false;
 	
 	Connection(Socket socket) throws IOException{
-		in = new DataInputStream(socket.getInputStream());
+            in = new DataInputStream(socket.getInputStream());
 	    out = new DataOutputStream(socket.getOutputStream());
 	    inreader = new BufferedReader( new InputStreamReader(in));
 	    outwriter = new PrintWriter(out, true);
@@ -39,50 +44,92 @@ public class Connection extends Thread {
 	 * returns true if the message was written, otherwise false
 	 */
 	public boolean writeMsg(String msg) {
-		if(open){
-			outwriter.println(msg);
-			outwriter.flush();
-			return true;	
-		}
-		return false;
+            if(open) {
+		outwriter.println(msg);
+		outwriter.flush();
+		return true;	
+            }
+            return false;
 	}
 	
 	public void closeCon(){
-		if(open){
-			log.info("closing connection "+Settings.socketAddress(socket));
-			try {
-				term=true;
-				inreader.close();
-				out.close();
-			} catch (IOException e) {
-				// already closed?
-				log.error("received exception closing the connection "+Settings.socketAddress(socket)+": "+e);
-			}
+            if(open){
+		log.info("closing connection "+Settings.socketAddress(socket));
+		try {
+                    term = true;
+                    inreader.close();
+                    out.close();
+		} catch (IOException e) {
+                    // already closed?
+                    log.error("received exception closing the connection "+Settings.socketAddress(socket)+": "+e);
 		}
+            }
 	}
 	
 	
 	public void run(){
-		try {
-			String data;
-			while(!term && (data = inreader.readLine())!=null){
-				term=Control.getInstance().process(this,data);
-			}
-			log.debug("connection closed to "+Settings.socketAddress(socket));
-			Control.getInstance().connectionClosed(this);
-			in.close();
-		} catch (IOException e) {
-			log.error("connection "+Settings.socketAddress(socket)+" closed with exception: "+e);
-			Control.getInstance().connectionClosed(this);
+            try {
+                String data;
+                
+                //wei
+                // debugger
+                log.debug("Processing data...");
+                // send an Authenticate command if remote hostname is supplied
+                if(Settings.getRemoteHostname()!=null) {
+                    this.sendAuthenticate();
+                }
+                
+		while(!term && (data = inreader.readLine())!= null){
+                    //wei
+                    log.info("Processing data: " + data);
+                    term = Control.getInstance().process(this,data);
 		}
-		open=false;
+                
+		log.debug("connection closed to "+Settings.socketAddress(socket));
+		Control.getInstance().connectionClosed(this);
+		in.close();
+                
+                // wei
+                socket.close();
+                
+            } catch (IOException e) {
+		log.error("connection "+Settings.socketAddress(socket)+" closed with exception: "+e);
+		Control.getInstance().connectionClosed(this);
+            }
+            open = false;
 	}
 	
 	public Socket getSocket() {
-		return socket;
+            return socket;
 	}
 	
 	public boolean isOpen() {
-		return open;
+            return open;
 	}
+        
+        //wei
+        public void sendAuthenticate() {
+            // create an authenticate json obj
+            JSONObject Authenticate = new JSONObject();
+            Authenticate.put("command", "AUTHENTICATE");
+            if (Settings.getSecret() != null) {
+                Authenticate.put("secret", Settings.getSecret());
+            }
+            this.writeMsg(Authenticate.toJSONString());
+            log.info("AUTHENTICATE SENT");
+        }
+        
+        public void sendAuthenticate_Fail(String secret) {
+            JSONObject Authenticate_Fail = new JSONObject();
+            Authenticate_Fail.put("command", "AUTHENTICATION_FAIL");
+            Authenticate_Fail.put("info", "the supplied secret is incorrect: " + secret);
+            this.writeMsg(Authenticate_Fail.toJSONString());
+        }
+        
+        public void sendInvalidMsg() {
+            JSONObject invalid_Msg = new JSONObject();
+            invalid_Msg.put("command", "INVALID_MESSAGE");
+            invalid_Msg.put("info", "invalid command");
+            this.writeMsg(invalid_Msg.toJSONString());
+        }
 }
